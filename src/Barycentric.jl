bc177732e5d8542f894ba82b5e7995531e6027b3
David AW Barton <david.barton@bristol.ac.uk>
2018-10-27 21:08:07 +0100

    Update to more Julian style

--- src/Barycentric.jl
+++ src/Barycentric.jl
@@ -2,148 +2,119 @@
 Barycentric
 ===========
 
-This module implements the Barycentric formula for polynomial interpolation on
+This package implements the Barycentric formula for polynomial interpolation on
 equispaced points and Chebyshev points of the first and second kind. The
 formulae used are taken from the paper of Berrut and Trefethen, SIAM Review,
 2004.
+
+Other packages that may be of interest are FastGaussQuadrature and ApproxFun.
 
 Written by David A.W. Barton (david.barton@bristol.ac.uk) 2016-2018 and licensed
 under the MIT license <https://opensource.org/licenses/MIT>
 """
 module Barycentric
 
-export points_chebyshev1, points_chebyshev2, points_equispaced, weights,
-    weights_chebyshev1, weights_chebyshev2, weights_equispaced, interpolate,
-    interpolation_matrix, differentiation_matrix
+export Chebyshev1, Chebyshev2, Equispaced, weights, nodes, interpolate, interpolation_matrix
 
-# Unless otherwise specified, equation references refer to Berrut and Trefethen, SIAM Review, 2004
+#-- Node distributions
 
-using DocStringExtensions
+abstract type AbstractPolynomial{N, T <: AbstractFloat} end
 
-"""
-$(SIGNATURES)
-
-Return an array of equispaced points with n intervals. For example
-`x = points_equispaced(10)`
-"""
-function points_equispaced(n::Integer, numtype=Float64)
-    # For completeness only
-    range(-one(numtype), stop=one(numtype), length=n+1)
-end
-
-"""
-$(SIGNATURES)
-
-Return an array of Chebyshev points of the first kind. For example
-`x = points_chebyshev1(10)`
-"""
-function points_chebyshev1(n::Integer, numtype=Float64)
-    cos.([numtype(2j + 1)*π/(2n + 2) for j = 0:n])
-end
-
-"""
-$(SIGNATURES)
-
-Return an array of Chebyshev points of the second kind. For example
-`x = points_chebyshev2(10)`
-"""
-function points_chebyshev2(n::Integer, numtype=Float64)
-    cos.([numtype(j)*π/n for j = 0:n])
-end
-
-"""
-$(SIGNATURES)
-
-Return the Barycentric weights for arbitrary points. For example
-`w = weights(linspace(0, 1, 11))`
-"""
-function weights(x::AbstractVector{T}) where {T <: AbstractFloat}
-    w = similar(x)
-    fill!(w, one(T))
-    nx = length(x)
-    for i ∈ eachindex(w, x)
-        x_i = x[i]
-        for j ∈ 1:i-1
-            w[i] *= x_i - x[j]
-        end
-        for j ∈ i+1:nx
-            w[i] *= x_i - x[j]
+for name in [:Chebyshev1, :Chebyshev2, :Equispaced]
+    @eval struct $name{N, T <: AbstractFloat} <: AbstractPolynomial{N, T}
+        shift::T
+        scale::T
+        nodes::Vector{T}
+        weights::Vector{T}
+        function $name{N, T}(start::T, stop::T) where {N, T <: AbstractFloat}
+            _shift = (stop + start)/2
+            _scale = (stop - start)/2
+            _nodes = nodes($name{N, T}, _shift, _scale)
+            _weights = weights($name{N, T}, _scale)
+            new{N, T, typeof(_nodes), typeof(_weights)}(_shift, _scale, _nodes, _weights)
         end
     end
-    1 ./ w
+    @eval $name{N, T}(start=-1, stop=1) where {N, T} = $name{N, T}(convert(T, start), convert(T, stop))
+    @eval $name{N}(start=-1, stop=1) where N = $name{N, Float64}(convert(Float64, start), convert(Float64, stop))
+    @eval (poly::$name)(y) = interpolate(poly, y)
+    @eval (poly::$name)(y, x) = interpolate(poly, y, x)
 end
 
 """
-$(SIGNATURES)
+    weights(poly)
 
-Return the Barycentric weights for equispaced points with n intervals. For
-example `w = weights_equispaced(10)`
+Return the Barycentric weights for the specified orthogonal polynomials.
 """
-function weights_equispaced(n::Integer, numtype=Float64)
-    # Eq. (5.1)
-    w = numtype[binomial(n, j) for j = 0:n]
-    if isodd(n)
-        w[1:2:end] .= -w[1:2:end]
-    else
-        w[2:2:end] .= -w[2:2:end]
-    end
-    return w
-end
+function weights end
 
-"""
-$(SIGNATURES)
+weights(poly::Type{<: AbstractPolynomial{N}}, scale) where N = [scale*_weight(poly, j) for j = 0:N]
 
-Return the Barycentric weights for Chebyshev points of the first kind. For
-example `w = weights_chebyshev1(10)`
-"""
-function weights_chebyshev1(n::Integer, numtype=Float64)
-    # Eq. (5.3)
-    w = [sin.(numtype(2j + 1)*π/(2n + 2)) for j = 0:n]
-    w[2:2:end] .= -w[2:2:end]
-    return w
-end
+# Eq. (5.1)
+@inline _weight(poly::Type{<: Equispaced{N, T}}, j::Integer) where {N, T} = T((2*xor(isodd(N), iseven(j))-1)*binomial(N, j))
+
+# Eq. (5.3)
+@inline _weight(poly::Type{<: Chebyshev1{N, T}}, j::Integer) where {N, T} = -(2*iseven(j)-1)*sinpi(T(2j + 1)/(2N + 2))
+
+# Eq. (5.4)
+@inline _weight(poly::Type{<: Chebyshev2{N, T}}, j::Integer) where {N, T} = -T((1.0 - 0.5*((j == 0) || (j == N)))*(2*iseven(j) - 1))
+
+weights(poly::AbstractPolynomial) = poly.weights
 
 """
-$(SIGNATURES)
+    nodes(poly)
 
-Return the Barycentric weights for Chebyshev points of the second kind. For
-example `w = weights_chebyshev2(10)`
+Return the nodes for the specified orthogonal polynomials.
 """
-function weights_chebyshev2(n::Integer, numtype=Float64)
-    # Eq. (5.4)
-    w = ones(numtype, n+1)
-    w[2:2:end] .= -1.0
-    w[1] = 0.5
-    w[end] *= 0.5
-    return w
-end
+function nodes end
+
+nodes(poly::Type{<: AbstractPolynomial{N}}, shift, scale) where N = [_node(poly, j)*scale + shift for j = 0:N]
+
+nodes(poly::Type{<: Equispaced{N}}, shift, scale) where N = range(shift - scale, stop=shift + scale, length=N+1)
+
+@inline _node(poly::Type{<: Chebyshev1{N, T}}, j::Integer) where {N, T} = -cospi(T(2j + 1)/(2N + 2))
+
+@inline _node(poly::Type{<: Chebyshev2{N, T}}, j::Integer) where {N, T} = -cospi(T(j)/N)
+
+nodes(poly::AbstractPolynomial) = poly.nodes
 
 """
-$(SIGNATURES)
+    interpolation_matrix(poly::AbstractPolynomial, x)
 
-Return the interpolation matrix from the points x to the points xnew using the
-Barycentric weights w. For example :
+Return the interpolation matrix from the nodes of `poly` to the point(s) `x`.
+For example :
 
-    x = points_chebyshev2(10)
-    w = weights_chebyshev2(10)
-    xnew = points_equispaced(5)
-    P = interpolation_matrix(xnew, x, w)
+    P = Chebyshev2{5}()
+    x = range(-1, stop=1, length=10)
+    M = interpolation_matrix(P, x)
 
-Now ynew ≈ `P*y` for some vector y.
+Now `y(x) ≈ M*y₀` given that `y(nodes(poly)) = y₀.
 """
-function interpolation_matrix(xnew::AbstractVector{T}, x::AbstractVector{T}, w::AbstractVector{T}) where {T <: AbstractFloat}
+function interpolation_matrix(poly::AbstractPolynomial{N, T}, x::Union{Number, AbstractVector}) where {N, T}
+    w = weights(poly)
+    x₀ = nodes(poly)
+    M = Matrix{T}(undef, length(x), N+1)
     # Eq. (4.2)
-    xdiff = xnew .- x'
-    M = w' ./ xdiff
-    Msum = sum(M, dims=2)
-    for i ∈ 1:size(M, 2)
-        for j ∈ 1:size(M, 1)
-            if Msum[j] == 0
-                M[j, i] = 0
-            elseif xdiff[j, i] == 0
-                M[j, i] = 1
-            else
-                M[j, i] /= Msum[j]
+    for j = eachindex(x)
+        xx = convert(T, x[j])
+        Msum = zero(T)
+        exact = 0
+        for i = Base.OneTo(N + 1)
+            exact = ifelse(xx == x₀[i], i, exact)
+            M[j, i] = w[i] / (xx - x₀[i])
+            Msum += M[j, i]
+        end
+        if Msum == 0
+            for i = Base.OneTo(N + 1)
+                M[j, i] = zero(T)
+            end
+        elseif exact > 0
+            for i = Base.OneTo(N + 1)
+                M[j, i] = zero(T)
+            end
+            M[j, exact] = one(T)
+        else
+            for i = Base.OneTo(N + 1)
+                M[j, i] /= Msum
             end
         end
     end
@@ -151,48 +122,36 @@ function interpolation_matrix(xnew::AbstractVector{T}, x::AbstractVector{T}, w::
 end
 
 """
-$(SIGNATURES)
+    interpolate(poly::AbstractPolynomial, y₀, [x])
 
-Return the interpolated values from the points x with the values y to the
-points xnew using the Barycentric weights w. For example :
-
-    x = points_chebyshev2(10)
-    w = weights_chebyshev2(10)
-    y = sin(π*x)
-    xnew = points_equispaced(5)
-    ynew = interpolate(xnew, x, y, w)
+Return the value of `y(x)` given that `y(nodes(poly)) = y₀.` If the value of `x`
+is not provided, return a function `y(x)` that evaluates the interpolant at any
+`x`.
 """
-function interpolate(xnew::AbstractVector{T}, x::AbstractVector{T}, y::AbstractVector{T}, w::AbstractVector{T}) where {T <: AbstractFloat}
-    return interpolation_matrix(xnew, x, w)*y
-end
-
-"""
-$(SIGNATURES)
-
-Return the differentiation matrix for the points x with the Barycentric weights
-w. For example :
-
-    x = points_chebyshev2(10)
-    w = weights_chebyshev2(10)
-    D = differentiation_matrix(x, w)
-
-Now dy/dx ≈ `D*y` for some vector y.
-"""
-function differentiation_matrix(x::AbstractVector{T}, w::AbstractVector{T}) where {T <: AbstractFloat}
-    # Eqs. (9.4) and (9.5)
-    nx = length(x)
-    D = (w' ./ w) ./ (x .- x')
-    for i ∈ 1:nx
-        Dsum = zero(eltype(x))
-        for j ∈ 1:i-1
-            Dsum += D[i, j]
-        end
-        for j ∈ i+1:nx
-            Dsum += D[i, j]
-        end
-        D[i, i] = -Dsum
+function interpolate(poly::AbstractPolynomial{N, T}, y₀::AbstractVector{<: Number}, x::Number) where {N, T}
+    w = weights(poly)
+    x₀ = nodes(poly)
+    xx = convert(T, x)
+    # Eq. (4.2)
+    numer = zero(T)
+    denom = zero(T)
+    exact = 0
+    for j = Base.OneTo(N+1)
+        xdiff = xx - x₀[j]
+        temp = w[j] / xdiff
+        numer += temp*convert(T, y₀[j])
+        denom += temp
+        exact = ifelse(xdiff == 0, j, exact)
     end
-    return D
+    if exact > 0
+        return convert(T, y₀[exact])
+    else
+        return numer/denom
+    end
 end
+
+interpolate(poly::AbstractPolynomial{N, T}, y₀::AbstractVector{<: Number}) where {N, T} = x -> interpolate(poly, y₀, x)
+interpolate(poly::AbstractPolynomial{N, T}, y₀::AbstractVector{<: Number}, x::AbstractVector{<: Number}) where {N, T} = [interpolate(poly, y₀, xᵢ) for xᵢ in x]
+
 
 end
